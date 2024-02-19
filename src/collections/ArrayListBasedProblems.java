92e172f3b27ce3201d6e7b3f4864b289f1acb67d
unknown <gangadharucet@gmail.com>
2024-02-19 19:22:50 +0530

    Add into list

---
+++ src/collections/ArrayListBasedProblems.java
@@ -0,0 +1,38 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ArrayListBasedProblems {
+
+	public static List<Integer> removeDuplicateUseJava8(List<Integer> list) {
+		return list.stream().distinct().collect(Collectors.toList());
+	}
+
+	public static List<Integer> removeDuplicateWithoutStreams(List<Integer> list) {
+		return new ArrayList<>(new HashSet<>(list));
+	}
+
+	public static void main(String[] args) {
+
+		List<Integer> list = new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+
+		list.add(1);
+		System.out.println(list);
+
+		System.out.println(removeDuplicateUseJava8(list));
+
+		System.out.println(list);
+
+		System.out.println(removeDuplicateWithoutStreams(list));
+
+		System.out.println(list);
+
+	}
+
+}
