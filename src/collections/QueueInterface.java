cd0633d0d77bd8f3e44438e91c4fdfcff05c4998
unknown <gangadharucet@gmail.com>
2024-02-20 12:39:00 +0530

    Added Queue Interface

--- src/collections/QueueInterface.java
+++ src/collections/QueueInterface.java
@@ -18,8 +18,16 @@ public class QueueInterface {
 	Examine	     |   element()	  |        E       peek()
 	*/
 	
+	
+	//Note : if we try to call remove() or element() method on empty Q we will get "NoSuchElementException"
+	//But , calling poll() or peek() on empty will not raise any exception
+	
 	public static void main(String[] args) {
 		Queue<Integer> q = new LinkedList<>();
+		
+		
+		q.peek();
+		q.poll();
 		
 		q.offer(1);
 		q.offer(2);
