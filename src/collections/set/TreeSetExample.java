9a83e306e03bc3886ded71167b68abf5db5e4d82
unknown <gangadharucet@gmail.com>
2024-02-21 13:16:42 +0530

    Added code for TreeSet

--- src/collections/set/TreeSetExample.java
+++ src/collections/set/TreeSetExample.java
@@ -6,13 +6,27 @@ import java.util.TreeSet;
 public class TreeSetExample {
 
 	public static void main(String[] args) {
+		
+		/*
+		 * TreeSet()
+			Constructs a new, empty tree set, sorted according to the natural ordering of its elements.
+			The object must be implementing Comparable<E> Interface
+			
+		  TreeSet(SortedSet<E> s)
+			Constructs a new tree set containing the same elements and using the same ordering as the specified sorted set.
+			
+	      TreeSet(Comparator<? super E> comparator)
+			Constructs a new, empty tree set, sorted according to the specified comparator.
+			
+		  TreeSet(Collection<? extends E> c)
+			Constructs a new tree set containing the elements in the specified collection, sorted according to the natural ordering of its elements.
+		 * 
+		 */
 
-		SortedSet<StringBuilder> tree = new TreeSet<>();
-		// The class storing in tree must be comparable , if we are using the default
-		// TreeSet constructor
+		SortedSet<String> tree = new TreeSet<>();
 
-		tree.add(new StringBuilder("Hi"));
-		System.out.println(tree);//[Hi]
+		tree.add(new String("Hi"));
+		System.out.println(tree);
 
 		// ClassCastException : Student cannot be cast to class java.lang.Comparable
 		SortedSet<Student> tree2 = new TreeSet<>();
