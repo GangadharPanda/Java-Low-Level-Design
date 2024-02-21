51027a9e11b2350b65bdfcaf83ef6076e7ebd83e
unknown <gangadharucet@gmail.com>
2024-02-21 14:40:06 +0530

    Added id inplace of age

--- src/collections/set/Student.java
+++ src/collections/set/Student.java
@@ -1,9 +1,11 @@
 package collections.set;
 
-public class Student {
+import java.util.Objects;
+
+public class Student implements Comparable<Student> {
 
 	private String name;
-	private int age;
+	private int id;
 
 	public String getName() {
 		return name;
@@ -13,11 +15,48 @@ public class Student {
 		this.name = name;
 	}
 
-	public int getAge() {
-		return age;
+	public int getId() {
+		return id;
 	}
 
-	public void setAge(int age) {
-		this.age = age;
+	public Student(String name, int id) {
+		super();
+		this.name = name;
+		this.id = id;
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		return id == other.id && Objects.equals(name, other.name);
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public int compareTo(Student o) {
+
+		System.out.println("this " + this);// THIS is the element which is to be inserted into the map
+		System.out.println("Parameter " + o);// Incoming parameter is the elements which are already saved into the tree
+		return this.name.compareTo(o.name);
+	}
+
+	@Override
+	public String toString() {
+		return "Student [name =" + name + ", id =" + id + "]";
+	}
+
 }
--- src/collections/set/TreeSetExample.java
+++ src/collections/set/TreeSetExample.java
@@ -7,30 +7,96 @@ public class TreeSetExample {
 
 	public static void main(String[] args) {
 		
+		// Note : TreeSet uses a self-balancing binary search tree (BST) is any node-based binary search tree
+		// that automatically keeps its height (maximal number of levels below the root) small in the face of 
+		// arbitrary item insertions and deletions.
+
 		/*
-		 * TreeSet()
-			Constructs a new, empty tree set, sorted according to the natural ordering of its elements.
-			The object must be implementing Comparable<E> Interface
-			
-		  TreeSet(SortedSet<E> s)
-			Constructs a new tree set containing the same elements and using the same ordering as the specified sorted set.
-			
-	      TreeSet(Comparator<? super E> comparator)
-			Constructs a new, empty tree set, sorted according to the specified comparator.
-			
-		  TreeSet(Collection<? extends E> c)
-			Constructs a new tree set containing the elements in the specified collection, sorted according to the natural ordering of its elements.
+		 * TreeSet() 
+		 * Constructs a new, empty tree set, sorted according to the natural
+		 * ordering of its elements. The object must be implementing Comparable<E>
+		 * Interface
+		 * 
+		 * TreeSet(SortedSet<E> s) 
+		 * Constructs a new tree set containing the same
+		 * elements and using the same ordering as the specified sorted set.
+		 * 
+		 * TreeSet(Comparator<? super E> comparator) 
+		 * Constructs a new, empty tree set,
+		 * sorted according to the specified comparator.
+		 * 
+		 * TreeSet(Collection<? extends E> c) 
+		 * Constructs a new tree set containing the
+		 * elements in the specified collection, sorted according to the natural
+		 * ordering of its elements.
 		 * 
 		 */
 
-		SortedSet<String> tree = new TreeSet<>();
-
-		tree.add(new String("Hi"));
+		SortedSet<Student> tree = new TreeSet<>();
+		tree.add(new Student("A", 1));
+		
+		System.out.println("-------One Comparison with A------------");
+		tree.add(new Student("B", 2));
+		System.out.println("-------Two Comparison with A & B-------");
+		tree.add(new Student("C", 3));
+		
+		
+		/**
+		 *  
+		 *  A 
+		 *   \ 
+		 *    B
+		 *     \ 
+		 *      C
+		 *      
+		 *  
+		 * Now the self balancing will happen 
+		 * 
+		 *    B
+		 *   / \
+		 *  A   C
+		 * 
+		 * 
+		 */
+		System.out.println("-------Two Comparison with B & C-------");
+		tree.add(new Student("D", 4));
+		
 		System.out.println(tree);
-
-		// ClassCastException : Student cannot be cast to class java.lang.Comparable
-		SortedSet<Student> tree2 = new TreeSet<>();
-		tree2.add(new Student());
+		/*
+		 * 
+		 *  this Student [name=A, id =]
+			Parameter Student [name=A, id = 1]
+			
+			While inserting B
+			-------One Comparison with A------------
+			
+			this Student [name=B, id = 2]
+			Parameter Student [name=A, id = 1]
+			
+			While inserting C
+			-------Two Comparison with A & B-------
+			
+			this Student [name=C, id = 3]
+			Parameter Student [name=A, id = 1]
+			this Student [name=C, id = 3]
+			Parameter Student [name=B, id = 2]
+			
+			While inserting D
+			-------Two Comparison with B & C-------
+			
+			this Student [name=D, id = 4]
+			Parameter Student [name=B, id = 2]
+			this Student [name=D, id = 4]
+			Parameter Student [name=C, id = 3]
+		 * 
+		 * 
+		 */
+		
+		
+		SortedSet<Integer> intTree = new TreeSet<>();
+		
+		
+		
 
 	}
 
