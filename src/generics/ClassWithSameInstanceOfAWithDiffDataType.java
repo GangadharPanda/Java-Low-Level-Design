d1762a18cd347ae2b1b3f14dc8010e89bb0b696a
Gangadhar Panda <gangadharucet@gmail.com>
2025-12-24 19:03:01 +0530

    Added some code for Wrapper Cache

--- src/generics/ClassWithSameInstanceOfAWithDiffDataType.java
+++ src/generics/ClassWithSameInstanceOfAWithDiffDataType.java
@@ -1,6 +1,12 @@
 package generics;
 
-public class ClassWithSameInstanceOfAWithDiffDataType implements InterfaceA<String>, InterfaceA<Integer>{
+public class ClassWithSameInstanceOfAWithDiffDataType implements InterfaceA<String>{
+    //,InterfaceA<Integer>
+
+    @Override
+    public void setData(String data) {
+
+    }
     // The Collision: After Type Erasure, both interfaces demand the exact same method signature
     // (void setData(Object)).
     // The JVM can not create two setData method with the same Object.
--- src/generics/ClassWithSimilarInterface.java
+++ src/generics/ClassWithSimilarInterface.java
@@ -1,15 +1,16 @@
 package generics;
 
-public class ClassWithSimilarInterface implements InterfaceA<String>, InterfaceB<Integer>{
+public class ClassWithSimilarInterface implements InterfaceA<String>{
     @Override
     public void setData(String data) {
 
     }
 
-    @Override
-    public void setData(Integer data) {
-
-    }
+    //, InterfaceB<Integer>
+//    @Override
+//    public void setData(Integer data) {
+//
+//    }
     // 'setData(T)' in 'generics.InterfaceA' clashes with 'setData(T)' in 'generics.InterfaceB';
     // both methods have same erasure, yet neither overrides the other
 }
